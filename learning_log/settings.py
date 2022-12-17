996ad04514777eb187cb020c18c20da9851c91d7
LMSerhii <leonovserhii89@gmail.com>
2022-12-17 15:34:19 +0200

    users add

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my_apps
     'learning_logs',
+    'users',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
